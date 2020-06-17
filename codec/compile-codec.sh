0e7edaa14d57e824607504033386b183e704c583
juanesf <juanesf91@gmail.com>
2020-06-16 22:07:31 -0400

    Add files
    - Add compiled folder
    - Add uncompiled folder
    - Add script for clone, compile and copy compiled module for sun8i-r40

---
+++ codec/compile-codec.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+###compile codec m2u
+echo "\033[36m Compile Codec for M2U(R40).\033[0m"
+echo "\033[36m downloading sources.\033[0m"
+git clone https://github.com/juanesf/codec
+echo "\033[36m changing folder.\033[0m"
+cd codec/uncompile
+echo "\033[36m compiling....\033[0m"
+make -C /lib/modules/$(uname -r)/build M=$(pwd) modules
+wait
+echo "\033[36m copy modules.\033[0m"
+sudo cp sun4i-codec.ko /lib/modules/$(uname -r)/kernel/sound/soc/sunxi
+
+##speaker-test -c2 -D hw:3,0
+##speaker-test -t wav -c2 -D hw:3,0
