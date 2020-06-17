34c1a8fdb59d561014bfe0a6f845c6f41f29cdf5
juanesf <juanesf91@gmail.com>
2020-06-16 22:14:50 -0400

    move folders
    move folders
    fix script

--- compile-codec.sh
+++ compile-codec.sh
@@ -4,12 +4,13 @@ echo "\033[36m Compile Codec for M2U(R40).\033[0m"
 echo "\033[36m downloading sources.\033[0m"
 git clone https://github.com/juanesf/codec
 echo "\033[36m changing folder.\033[0m"
-cd codec/uncompile
+cd codec/uncompiled
 echo "\033[36m compiling....\033[0m"
 make -C /lib/modules/$(uname -r)/build M=$(pwd) modules
 wait
 echo "\033[36m copy modules.\033[0m"
 sudo cp sun4i-codec.ko /lib/modules/$(uname -r)/kernel/sound/soc/sunxi
 
+##speeaker-test -t wav -c 2 -D hw:0,0
 ##speaker-test -c2 -D hw:3,0
 ##speaker-test -t wav -c2 -D hw:3,0
