83c41f323644cd4b8b638287a998bbc31fc83ac7
juanesf <juanesf91@gmail.com>
2020-06-16 22:29:56 -0400

    Update compile-codec.sh

--- compile-codec.sh
+++ compile-codec.sh
@@ -1,18 +1,19 @@
 #!/usr/bin/env bash
-###compile codec m2u
-echo "\033[36m Compile Codec for M2U(R40).\033[0m"
-echo "\033[36m downloading sources.\033[0m"
+### compile codec m2u
+echo Compile Codec for M2U(R40).
+echo Downloading sources.
 git clone https://github.com/juanesf/codec
 wait
-echo "\033[36m changing folder.\033[0m"
+echo Changing folder.
 cd codec
 cd uncompiled
-echo "\033[36m compiling....\033[0m"
+echo Compiling...
 make -C /lib/modules/$(uname -r)/build M=$(pwd) modules
 wait
-echo "\033[36m copy modules.\033[0m"
+echo Copy modules.
 sudo cp sun4i-codec.ko /lib/modules/$(uname -r)/kernel/sound/soc/sunxi
-
-##speeaker-test -t wav -c 2 -D hw:0,0
-##speaker-test -c2 -D hw:3,0
-##speaker-test -t wav -c2 -D hw:3,0
+echo Done
+exit 0
+### speeaker-test -t wav -c 2 -D hw:0,0
+### speaker-test -c2 -D hw:3,0
+### speaker-test -t wav -c2 -D hw:3,0
