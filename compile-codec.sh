5305fd0e1ba3f8f4d1ce3e03522d9768ad26dd2c
juanesf <juanesf91@gmail.com>
2020-06-16 22:24:03 -0400

    Update compile-codec.sh

--- compile-codec.sh
+++ compile-codec.sh
@@ -3,8 +3,10 @@
 echo "\033[36m Compile Codec for M2U(R40).\033[0m"
 echo "\033[36m downloading sources.\033[0m"
 git clone https://github.com/juanesf/codec
+wait
 echo "\033[36m changing folder.\033[0m"
-cd codec/uncompiled
+cd codec
+cd uncompiled
 echo "\033[36m compiling....\033[0m"
 make -C /lib/modules/$(uname -r)/build M=$(pwd) modules
 wait
