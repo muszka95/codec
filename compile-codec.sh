b3fb5322966cfe50bd2046ec2b49b31ab7861798
juanesf <juanesf91@gmail.com>
2020-06-16 22:35:05 -0400

    Update compile-codec.sh

--- compile-codec.sh
+++ compile-codec.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 ###compile codec m2u
-echo Compile Codec for M2U(R40).
-echo Downloading sources.
+###Compile Codec for M2U(R40).
+###Downloading sources.
 git clone https://github.com/juanesf/codec
 wait
-echo Changing folder.
+###Changing folder.
 cd codec
 cd uncompiled
-echo Compiling...
+###Compiling...
 make -C /lib/modules/$(uname -r)/build M=$(pwd) modules
 wait
-echo Copy modules.
+###Copy modules.
 sudo cp sun4i-codec.ko /lib/modules/$(uname -r)/kernel/sound/soc/sunxi
-echo Done
+
 ###sudo modprobe sun4i-codec
 ###aplay -l
 ###dmesg | grep -i codec
